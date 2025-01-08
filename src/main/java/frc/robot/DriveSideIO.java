35fa118101594603304d895b34292221b420f3ca
reecelikesramen <3149587+reecelikesramen@users.noreply.github.com>
2025-01-08 15:21:23 -0600

    Create a hardware abstraction for a side of the differential drive
    +DriveSideIO.java:
    - IO inputs also include critical data for diagnostics like: voltage, current, connected state.
    - Also, IO inputs include odometry information like: positions recorded and corresponding timestamps
    - We __always__ include a `updateInputs` stub method for implementations to update inputs every periodic cycle
    - Then we add methods that will satisfy the low-level control requests of the subsystem. In our case: set drive side motor(s) to a voltage, and set drive side motor(s) to target an angular velocity

---
+++ src/main/java/frc/robot/DriveSideIO.java
@@ -0,0 +1,41 @@
+package frc.robot;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Time;
+import edu.wpi.first.units.measure.Voltage;
+
+public interface DriveSideIO {
+
+    @AutoLog
+    public static class DriveSideIOInputs {
+        public boolean connected = false;
+        public Angle position = Radians.of(0.0);
+        public AngularVelocity velocity = RadiansPerSecond.of(0.0);
+        public Voltage appliedVolts = Volts.of(0.0);
+        public Current current = Amps.of(0.0);
+
+        public Time[] odometryTimestamps = new Time[] {};
+        public Angle[] odometryPositions = new Angle[] {};
+    }
+
+    /** Updates the set of loggable inputs. */
+    public default void updateInputs(DriveSideIOInputs inputs) {
+    }
+
+    /** Run the drive side at the specified open loop value. */
+    public default void setDriveOpenLoop(Voltage output) {
+    }
+
+    /** Run the drive side at the specified velocity. */
+    public default void setDriveVelocity(AngularVelocity velocity) {
+    }
+}
